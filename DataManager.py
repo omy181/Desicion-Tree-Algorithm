12fde76e53bacb4c0229757a33f99a9e1b24094e
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2023-11-02 15:02:13 +0300

    possible splits, split,  entropy

--- DataManager.py
+++ DataManager.py
@@ -14,30 +14,51 @@ def IsNearPure(Data):
 
 def GetPossibleSplits(Data):
 
-    classes,counts = numpy.unique(Data[:,2],return_counts=True)
+    split_columns = {}
 
-    # Classification data
+    for column in range( (len(TrainSet.columns)-1)):
+        classes = numpy.unique(Data[:,column])
+
+        if isinstance(classes[0],str):
+            # Classification data
+            split_columns[column] = classes        
+            
+        else:
+            # Numerical data
+            mean_classes = []
+            for i in range(len(classes)-1):
+                mean_classes.append((classes[i] + classes[i+1])/2)
+              
+            split_columns[column] = mean_classes
     
-        
-
-    # Numerical data
-
-
-    #mostfrequent = classes[numpy.argmax(counts)]
-    
-
-
-    return 
+    return split_columns
       
-def CalculateEntropy():
-    return
+def CalculateEntropy(Data):
+    _,Counts = numpy.unique(Data[:,-1],return_counts=True)
+
+    p = Counts/ Counts.sum()
+    
+    return sum(p * (-numpy.log2(p)))
 
 def FindLowestEntropy():
     return
 
-def Split(Data):
+def Split(Data,Split_Column,Split_Value):
+
+    classes = numpy.unique(Data[:,Split_Column])
+
+    column_values = Data[:,Split_Column]
 
 
+    if isinstance(classes[0],str):
+        # for Categorical
+        above = TrainSet[classes[Split_Value] == column_values]
+        bellow = TrainSet[classes[Split_Value] != column_values]
 
-    return
+    else:
+        # for Numerical
+        above = TrainSet[Split_Value >= column_values]
+        bellow = TrainSet[Split_Value < column_values]    
+
+    return above,bellow
 
--- Testing.py
+++ Testing.py
@@ -5,5 +5,15 @@ tree = CreateTree()
 
 TrainData = numpy.array(TrainSet)
     
-print(GetPossibleSplits(TrainData))
+print(CalculateEntropy(TrainData))
 
+
+
+
+
+#print(Split(TrainData,1,48.25)) numerical 1st column element 48.25
+
+#print(Split(TrainData,0,0)) cathegorical 0th column and 0th element(a)
+
+
+#mostfrequent = classes[numpy.argmax(counts)]
